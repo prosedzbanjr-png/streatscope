eb16354038ddc70f0e8a0947f170f7a0f2235cf2
prosedzbanjr-png <prosedzbanjr@gmail.com>
2026-08-20 01:55:00 +0200

    Remove auction winner DOM mutation loop

--- app/market/[id]/layout.tsx
+++ app/market/[id]/layout.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from "react";
 import "../auction-access.css";
-import WinnerBanner from "./winner-banner";
 
 export default function VehicleLayout({children}:{children:ReactNode}){
-  return <><WinnerBanner/>{children}</>;
+  return children;
 }
