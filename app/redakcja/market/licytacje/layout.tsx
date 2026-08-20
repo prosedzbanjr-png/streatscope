af566e539e00025267b8ad1e1c502099a7ca65c0
prosedzbanjr-png <prosedzbanjr@gmail.com>
2026-08-20 12:52:23 +0200

    Remove auction admin DOM enhancer causing freezes

--- app/redakcja/market/licytacje/layout.tsx
+++ app/redakcja/market/licytacje/layout.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from "react";
 import "../auction-access-admin.css";
-import AuctionPanelEnhancer from "./auction-panel-enhancer";
 
 export default function AuctionAdminLayout({children}:{children:ReactNode}){
-  return <><AuctionPanelEnhancer/>{children}</>;
+  return children;
 }
