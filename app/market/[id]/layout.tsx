633ec864f8899e1207138cac7babaa970832e151
prosedzbanjr-png <prosedzbanjr@gmail.com>
2026-08-20 12:19:54 +0200

    Load Tow & Trade employee badge styles

--- app/market/[id]/layout.tsx
+++ app/market/[id]/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import "../auction-access.css";
 import "../auction-winner.css";
+import "../listed-by.css";
 import ListedBy from "./listed-by";
 
 export default function VehicleLayout({children}:{children:ReactNode}){
