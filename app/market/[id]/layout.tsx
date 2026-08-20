4f494077540d26801b1c9ce626684a32f7d2a8de
prosedzbanjr-png <prosedzbanjr@gmail.com>
2026-08-20 12:19:42 +0200

    Show employee who listed Tow & Trade vehicle

--- app/market/[id]/layout.tsx
+++ app/market/[id]/layout.tsx
@@ -1,7 +1,8 @@
 import type { ReactNode } from "react";
 import "../auction-access.css";
 import "../auction-winner.css";
+import ListedBy from "./listed-by";
 
 export default function VehicleLayout({children}:{children:ReactNode}){
-  return children;
+  return <>{children}<ListedBy/></>;
 }
