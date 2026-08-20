20a6f3b640ddffcc6363bf30cbc0674a96196f62
prosedzbanjr-png <prosedzbanjr@gmail.com>
2026-08-20 02:22:15 +0200

    Load auction winner styles

--- app/market/[id]/layout.tsx
+++ app/market/[id]/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "../auction-access.css";
+import "../auction-winner.css";
 
 export default function VehicleLayout({children}:{children:ReactNode}){
   return children;
