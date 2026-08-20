8787cf55a22247262d668756838acc79f4e9ba66
prosedzbanjr-png <prosedzbanjr@gmail.com>
2026-08-20 14:04:03 +0200

    Remove crashing auction realtime helper

--- app/market/[id]/layout.tsx
+++ app/market/[id]/layout.tsx
@@ -3,8 +3,7 @@ import "../auction-access.css";
 import "../auction-winner.css";
 import "../listed-by.css";
 import ListedBy from "./listed-by";
-import RealtimeAuctionEvents from "./realtime-auction-events";
 
 export default function VehicleLayout({children}:{children:ReactNode}){
-  return <>{children}<RealtimeAuctionEvents/><ListedBy/></>;
+  return <>{children}<ListedBy/></>;
 }
