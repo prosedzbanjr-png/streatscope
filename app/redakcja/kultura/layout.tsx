118bb244050244700427426d519536e5becfebd5
prosedzbanjr-png <prosedzbanjr@gmail.com>
2026-08-27 12:20:11 +0200

    enable review discord notifier for culture

--- app/redakcja/kultura/layout.tsx
+++ app/redakcja/kultura/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import LbPhonePublishToggle from "../lb-phone-publish-toggle";
+import ReviewDiscordNotifier from "../review-discord-notifier";
 
 export default function CultureEditorLayout({ children }: { children: ReactNode }) {
-  return <>{children}<LbPhonePublishToggle mode="culture" /></>;
+  return <>{children}<ReviewDiscordNotifier mode="culture" /><LbPhonePublishToggle mode="culture" /></>;
 }
