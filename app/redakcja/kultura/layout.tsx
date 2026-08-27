e2e44c20d43faa094703fb4b34e6829e6fdf8d1b
prosedzbanjr-png <prosedzbanjr@gmail.com>
2026-08-27 12:48:00 +0200

    add hidden preview control to culture editor

--- app/redakcja/kultura/layout.tsx
+++ app/redakcja/kultura/layout.tsx
@@ -1,7 +1,8 @@
 import type { ReactNode } from "react";
 import LbPhonePublishToggle from "../lb-phone-publish-toggle";
 import ReviewDiscordNotifier from "../review-discord-notifier";
+import HiddenPreviewButton from "../hidden-preview-button";
 
 export default function CultureEditorLayout({ children }: { children: ReactNode }) {
-  return <>{children}<ReviewDiscordNotifier mode="culture" /><LbPhonePublishToggle mode="culture" /></>;
+  return <>{children}<HiddenPreviewButton mode="culture" /><ReviewDiscordNotifier mode="culture" /><LbPhonePublishToggle mode="culture" /></>;
 }
