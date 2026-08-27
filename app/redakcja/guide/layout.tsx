d5ea4de8cb206b09d4c63d73a4b9f143980747a3
prosedzbanjr-png <prosedzbanjr@gmail.com>
2026-08-27 12:48:08 +0200

    add hidden preview control to guide editor

--- app/redakcja/guide/layout.tsx
+++ app/redakcja/guide/layout.tsx
@@ -1,7 +1,8 @@
 import type { ReactNode } from "react";
 import LbPhonePublishToggle from "../lb-phone-publish-toggle";
 import ReviewDiscordNotifier from "../review-discord-notifier";
+import HiddenPreviewButton from "../hidden-preview-button";
 
 export default function GuideEditorLayout({ children }: { children: ReactNode }) {
-  return <>{children}<ReviewDiscordNotifier mode="guide" /><LbPhonePublishToggle mode="guide" /></>;
+  return <>{children}<HiddenPreviewButton mode="guide" /><ReviewDiscordNotifier mode="guide" /><LbPhonePublishToggle mode="guide" /></>;
 }
