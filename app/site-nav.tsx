cf69e0c12a5f3eebedb5e9e3887d1a836a835fb8
prosedzbanjr-png <prosedzbanjr@gmail.com>
2026-09-19 20:28:30 +0200

    Add Charts to navigation

--- app/site-nav.tsx
+++ app/site-nav.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
 
 export function SiteNav() {
-  return <header className="article-nav"><Link href="/" className="wordmark">STREET<span>SCOPE</span></Link><nav className="sub-nav"><Link href="/wiadomosci">WIADOMOŚCI</Link><Link href="/fashion">FASHION</Link><Link href="/motor">MOTOR</Link><Link href="/miasto">MIASTO</Link><Link href="/o-redakcji">O REDAKCJI</Link></nav></header>;
+  return <header className="article-nav"><Link href="/" className="wordmark">STREET<span>SCOPE</span></Link><nav className="sub-nav"><Link href="/wiadomosci">WIADOMOŚCI</Link><Link href="/fashion">FASHION</Link><Link href="/motor">MOTOR</Link><Link href="/miasto">MIASTO</Link><Link href="/charts">CHARTS</Link><Link href="/o-redakcji">O REDAKCJI</Link></nav></header>;
 }
